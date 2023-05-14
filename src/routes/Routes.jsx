7d5998e1abf789f20a8d5e3590eeec955c475223
jawad Bin Azam <abidibnazam@gmail.com>
2023-05-14 10:46:46 +0600

    set up completed

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+        {
+            path: '/',
+            element: <Home></Home>
+        }
+      ]
+    },
+  ]);
+
+  export default router;
